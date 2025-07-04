aed88301f974ff0efa018c9a9c29a779b4121cdc
laurine <laurine.beduneau@epitech.eu>
2025-07-04 14:42:10 +0200

    Feat : modification main file of front-version folder

--- front-version/src/main.cpp
+++ front-version/src/main.cpp
@@ -4,69 +4,94 @@
 #include <sstream>
 #include <cstdlib>
 #include <ctime>
+
 #include "../shared/password_generator.hpp"
 
-// Fonction de génération (simplifiée ici pour tester)
-std::string generatePassword(int length) {
-    std::string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    std::string password = "";
-    std::srand(std::time(nullptr));
-
-    for (int i = 0; i < length; ++i) {
-        password += chars[rand() % chars.length()];
-    }
-
-    return password;
-}
-
 int main() {
-    sf::RenderWindow window(sf::VideoMode(600, 300), "Generateur de mots de passe");
-    sf::Font font;
+    sf::RenderWindow window(sf::VideoMode(600, 400), "Generateur de mots de passe");
 
+    sf::Font font;
     if (!font.loadFromFile("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf")) {
-        std::cerr << "Erreur de chargement de la police !" << std::endl;
+        std::cerr << "Erreur chargement police !" << std::endl;
         return 1;
     }
 
-    // Titre
-    sf::Text title("Generateur de mots de passe", font, 24);
-    title.setFillColor(sf::Color::Black);
-    title.setPosition(100, 20);
+    // Couleurs
+    sf::Color bgColor(245, 245, 250);
+    sf::Color boxColor(255, 255, 255);
+    sf::Color outlineColor(200, 200, 200);
+    sf::Color primaryColor(70, 130, 180);      // Steel blue
+    sf::Color hoverColor(90, 150, 200);
+    sf::Color successColor(100, 200, 100);
+    sf::Color textColor(30, 30, 30);
 
-    // Champ de saisie (affichage visuel)
+    // Titre
+    sf::Text title("Generateur de mot de passe", font, 26);
+    title.setFillColor(textColor);
+    sf::FloatRect titleBounds = title.getLocalBounds();
+    title.setPosition(300 - titleBounds.width / 2, 20);
+
+    // Champ de saisie
     sf::RectangleShape inputBox(sf::Vector2f(200, 40));
     inputBox.setPosition(200, 80);
-    inputBox.setFillColor(sf::Color::White);
-    inputBox.setOutlineColor(sf::Color::Black);
-    inputBox.setOutlineThickness(2);
+    inputBox.setFillColor(boxColor);
+    inputBox.setOutlineColor(outlineColor);
+    inputBox.setOutlineThickness(1);
 
     sf::Text inputText("", font, 20);
-    inputText.setFillColor(sf::Color::Black);
+    inputText.setFillColor(textColor);
     inputText.setPosition(210, 85);
     std::string inputString = "";
 
-    // Bouton
-    sf::RectangleShape generateButton(sf::Vector2f(150, 40));
-    generateButton.setPosition(225, 140);
-    generateButton.setFillColor(sf::Color(100, 150, 250));
+    // Cases à cocher
+    bool useLetters = true;
+    bool useNumbers = true;
+    bool useSymbols = true;
+
+    auto createCheckbox = [&](float x, float y, const std::string& label, sf::RectangleShape& box, sf::Text& text) {
+        box.setSize(sf::Vector2f(20, 20));
+        box.setPosition(x, y);
+        box.setFillColor(successColor);
+        box.setOutlineColor(outlineColor);
+        box.setOutlineThickness(1);
+
+        text.setFont(font);
+        text.setString(label);
+        text.setCharacterSize(16);
+        text.setFillColor(textColor);
+        text.setPosition(x + 30, y - 2);
+    };
+
+    sf::RectangleShape lettersBox, numbersBox, symbolsBox;
+    sf::Text lettersText, numbersText, symbolsText;
+    createCheckbox(200, 140, "Lettres", lettersBox, lettersText);
+    createCheckbox(200, 180, "Chiffres", numbersBox, numbersText);
+    createCheckbox(200, 220, "Symboles", symbolsBox, symbolsText);
+
+    // Bouton générer
+    sf::RectangleShape generateButton(sf::Vector2f(160, 45));
+    generateButton.setPosition(220, 270);
+    generateButton.setFillColor(primaryColor);
 
     sf::Text buttonText("Generer", font, 20);
     buttonText.setFillColor(sf::Color::White);
-    buttonText.setPosition(250, 145);
+    sf::FloatRect buttonBounds = buttonText.getLocalBounds();
+    buttonText.setPosition(220 + (160 - buttonBounds.width) / 2, 278);
 
-    // Texte du mot de passe généré
-    sf::Text passwordText("", font, 20);
-    passwordText.setFillColor(sf::Color::Blue);
-    passwordText.setPosition(100, 200);
+    // Mot de passe généré
+    sf::Text passwordText("", font, 18);
+    passwordText.setFillColor(sf::Color(50, 50, 100));
+    passwordText.setPosition(100, 330);
 
     while (window.isOpen()) {
         sf::Event event;
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
         while (window.pollEvent(event)) {
-            // Fermer la fenêtre
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            // Gérer la saisie de texte
+            // Clavier
             if (event.type == sf::Event::TextEntered) {
                 if (isdigit(event.text.unicode) && inputString.size() < 3) {
                     inputString += static_cast<char>(event.text.unicode);
@@ -77,27 +102,54 @@ int main() {
                 }
             }
 
-            // Gérer le clic sur le bouton
-            if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    auto mouseX = event.mouseButton.x;
-                    auto mouseY = event.mouseButton.y;
+            // Souris
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                float x = event.mouseButton.x;
+                float y = event.mouseButton.y;
 
-                    if (generateButton.getGlobalBounds().contains(mouseX, mouseY)) {
-                        int len = inputString.empty() ? 0 : std::stoi(inputString);
-                        if (len > 0) {
-                            std::string pwd = generatePassword(len);
-                            passwordText.setString("Mot de passe : " + pwd);
-                        }
+                if (lettersBox.getGlobalBounds().contains(x, y)) {
+                    useLetters = !useLetters;
+                    lettersBox.setFillColor(useLetters ? successColor : boxColor);
+                }
+                if (numbersBox.getGlobalBounds().contains(x, y)) {
+                    useNumbers = !useNumbers;
+                    numbersBox.setFillColor(useNumbers ? successColor : boxColor);
+                }
+                if (symbolsBox.getGlobalBounds().contains(x, y)) {
+                    useSymbols = !useSymbols;
+                    symbolsBox.setFillColor(useSymbols ? successColor : boxColor);
+                }
+
+                if (generateButton.getGlobalBounds().contains(x, y)) {
+                    int len = inputString.empty() ? 0 : std::stoi(inputString);
+                    if (len > 0 && (useLetters || useNumbers || useSymbols)) {
+                        std::string pwd = generatePassword(len, useLetters, useNumbers, useSymbols);
+                        passwordText.setString("Mot de passe : " + pwd);
+                        std::cout << "Mot de passe : " << pwd << std::endl;
+                    } else {
+                        passwordText.setString("Veuillez entrer une longueur valide et cocher au moins une option.");
                     }
                 }
             }
         }
 
-        window.clear(sf::Color::White);
+        // Hover effet sur le bouton
+        if (generateButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+            generateButton.setFillColor(hoverColor);
+        } else {
+            generateButton.setFillColor(primaryColor);
+        }
+
+        // Affichage
+        window.clear(bgColor);
         window.draw(title);
         window.draw(inputBox);
         window.draw(inputText);
+
+        window.draw(lettersBox); window.draw(lettersText);
+        window.draw(numbersBox); window.draw(numbersText);
+        window.draw(symbolsBox); window.draw(symbolsText);
+
         window.draw(generateButton);
         window.draw(buttonText);
         window.draw(passwordText);
